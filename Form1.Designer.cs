8ee1eb0fc81649696ee428cd707764b7eed25f7d
André Luis Sehnem <als10andr@hotmail.com>
2023-01-19 10:28:22 -0300

    First

---
+++ Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Geolocation3
+{
+    partial class frm_Principal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.painel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // painel
+            // 
+            this.painel.Location = new System.Drawing.Point(2, 34);
+            this.painel.Name = "painel";
+            this.painel.Size = new System.Drawing.Size(797, 416);
+            this.painel.TabIndex = 0;
+            // 
+            // frm_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.painel);
+            this.Name = "frm_Principal";
+            this.Text = "Geolocalização";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel painel;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+
+namespace Geolocation3
+{
+    public partial class frm_Principal : Form
+    {
+        public frm_Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //primeiro precisa https://www.nuget.org/packages/CefSharp.WinForms/#readme-body-tab
+            var browser = new ChromiumWebBrowser("https://www.google.com");
+            //se der erro aqui https://www.nuget.org/packages/CefSharp.Common.NETCore/
+            //this.Controls.Add(browser);
+            painel.Controls.Add(browser);
+
+        }
+    }
+}
